8357b9adb1d67fe8e62963b7d71f506809878bf4
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-04-13 23:57:28 -0700

    Set difference and subset'

---
+++ js/fcc/data_structures/setDifference.js
@@ -0,0 +1,68 @@
+function Set() {
+  var collection = [];
+
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  this.values = function () {
+    return collection;
+  };
+
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function (element) {
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.union = function (otherSet) {
+    var unionSet = new Set();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function (otherSet) {
+    var intersectionSet = new Set();
+    var firstSet = this.values();
+    firstSet.forEach(function (e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+    });
+    return intersectionSet;
+  };
+
+  this.difference = function (otherSet) {
+    const difference = new Set();
+    const otherSetValues = otherSet.values();
+    for (const el of collection) {
+      if (otherSetValues.indexOf(el) === -1) {
+        difference.add(el);
+      }
+    }
+
+    return difference;
+  };
+}
