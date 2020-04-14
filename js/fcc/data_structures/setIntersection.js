b259ea60a7156c87752314b449f08ef5bfa0019c
Bryce McMath <bryce.j.mcmath@gmail.com>
2020-04-13 23:48:26 -0700

    Set intersection

---
+++ js/fcc/data_structures/setIntersection.js
@@ -0,0 +1,56 @@
+function Set() {
+  var collection = [];
+
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+
+  this.values = function () {
+    return collection;
+  };
+
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+
+  this.remove = function (element) {
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  this.size = function () {
+    return collection.length;
+  };
+
+  this.union = function (otherSet) {
+    var unionSet = new Set();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function (e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function (otherSet) {
+    const intersect = new Set();
+    for (const el of otherSet.values()) {
+      if (collection.indexOf(el) !== -1) {
+        intersect.add(el);
+      }
+    }
+
+    return intersect;
+  };
+}
